df905ee7e636908182a103593862aef17ddcc81b
krisbebb <krisbebb@gmail.com>
2018-05-19 15:36:15 +1000

    class article

---
+++ lib/models/article.php
@@ -0,0 +1,58 @@
+<?php
+class Article extends Database{
+
+
+   public function is_db_empty(){
+       $is_empty = false;
+       try{
+          $query = "SELECT id FROM articles WHERE id=?";
+          if($statement = $this->prepare($query)){
+             $id=1;
+             $binding = array($id);
+             if(!$statement -> execute($binding)){
+                     throw new Exception("Could not execute query.");
+             }
+             else{
+                $result = $statement->fetch(PDO::FETCH_ASSOC);
+                if(empty($result)){
+                  $is_empty = true;
+                }
+             }
+          }
+          else{
+                throw new Exception("Problem with statement.");
+          }
+
+       }
+       catch(Exception $e){
+          throw new Exception($e->getMessage());
+       }
+       return $is_empty;
+
+    }
+
+    public function get_articles(){
+       try{
+          $query = "SELECT id, headline FROM articles";
+          if($statement = $this->prepare($query)){
+             $binding = array();
+             if(!$statement -> execute($binding)){
+                 throw new Exception("Could not execute query.");
+             }
+             else{
+                $results = $statement->fetchall(PDO::FETCH_ASSOC);
+                return $results;
+             }
+          }
+          else{
+            throw new Exception("Could not prepare statement.");
+
+          }
+       }
+       catch(Exception $e){
+           throw new Exception($e->getMessage());
+       }
+    }
+
+
+  
--- lib/views/home.php
+++ lib/views/home.php
@@ -1,3 +1,34 @@
 <h1>The Austro-Asian Times</h1>
 
 <p><?php echo $message ?></p>
+
+<?php
+if(!empty($users)){
+   echo "<table border='1'>";
+   echo "<tr><th>ID</th> <th>Name</th><th colspan='3'>Actions</th></tr>";
+   foreach($users As $user){
+          echo "<tr><td>{$user['id']}</td> <td>{$user['name']}</td>
+          <td>
+             <form action='/member/{$user['id']}' method='GET'>
+                <input type='submit' value='Show' />
+             </form>
+
+
+          </td>
+          <td>
+             <form action='/members/{$user['id']}' method='POST'>
+                <input type='hidden' name='_method' value='delete' />
+                <input class='delete' type='submit' value='Delete' />
+             </form>
+          </td>
+
+
+          </tr>";
+       }
+       echo "</table>";
+}
+else{
+  echo "<p>No Members yet</p>";
+}
+
+?>
