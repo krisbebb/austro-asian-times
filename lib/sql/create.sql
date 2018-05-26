6363f90021a72d8963cb21fabff061f3a37bedb7
krisbebb <krisbebb@gmail.com>
2018-05-26 16:03:07 +1000

    remove old sql files, add create and load

--- lib/sql/create.sql
+++ lib/sql/create.sql
@@ -1,24 +1,73 @@
--- phpMyAdmin SQL Dump
--- version 4.7.7
--- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: May 23, 2018 at 04:16 AM
--- Server version: 5.6.38
--- PHP Version: 7.2.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
---
--- Database: `news_db`
+-- Table structure for table `journalists`
 --
 
--- --------------------------------------------------------
+CREATE TABLE `journalists` (
+  `id` int(11) NOT NULL,
+  `login` varchar(15) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `privilege` int(11) NOT NULL DEFAULT '1',
+  `hashed_password` char(64) NOT NULL,
+  `salt` char(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-
+-- Indexes for table `journalists`
+--
+ALTER TABLE `journalists`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `login` (`login`);
 
 --
--- Table structure for table `articles`
+-- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `journalists`
+--
+- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+ALTER TABLE `journalists`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+  DROP TABLE IF EXISTS `tags`;
+  CREATE TABLE `tags` (
+    `tag_id` int(11) NOT NULL,
+    `tag_text` char(50) NOT NULL
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+  --
+
+  --
+  -- Indexes for dumped tables
+  --
+
+  --
+  -- Indexes for table `tags`
+  --
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`tag_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `tag_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+
+  -- --------------------------------------------------------
+
+  --
+  -- Table structure for table `articles`
+  --
 
 CREATE TABLE `articles` (
   `article_id` int(11) NOT NULL,
@@ -29,73 +78,12 @@ CREATE TABLE `articles` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `article_tags`
---
-
-CREATE TABLE `article_tags` (
-  `article_id` int(11) NOT NULL,
-  `tag_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `journalists`
---
-
-CREATE TABLE `journalists` (
-  `id` int(11) NOT NULL,
-  `login` varchar(15) NOT NULL,
-  `firstname` varchar(255) NOT NULL,
-  `lastname` varchar(255) NOT NULL,
-  `privilege` int(11) NOT NULL,
-  `hashed_password` char(64) NOT NULL,
-  `salt` char(16) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tags`
---
-
-CREATE TABLE `tags` (
-  `tag_id` int(11) NOT NULL,
-  `tag_text` char(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Indexes for dumped tables
---
-
---
 -- Indexes for table `articles`
 --
 ALTER TABLE `articles`
   ADD PRIMARY KEY (`article_id`),
+  ADD UNIQUE KEY `headline` (`headline`),
   ADD KEY `created_by_fk1` (`created_by`);
-
---
--- Indexes for table `article_tags`
---
-ALTER TABLE `article_tags`
-  ADD PRIMARY KEY (`article_id`,`tag_id`),
-  ADD KEY `tag_id` (`tag_id`) USING BTREE;
-
---
--- Indexes for table `journalists`
---
-ALTER TABLE `journalists`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tags`
---
-ALTER TABLE `tags`
-  ADD PRIMARY KEY (`tag_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -105,19 +93,7 @@ ALTER TABLE `tags`
 -- AUTO_INCREMENT for table `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `article_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `journalists`
---
-ALTER TABLE `journalists`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `tags`
---
-ALTER TABLE `tags`
-  MODIFY `tag_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `article_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
@@ -128,6 +104,32 @@ ALTER TABLE `tags`
 --
 ALTER TABLE `articles`
   ADD CONSTRAINT `created_by_fk1` FOREIGN KEY (`created_by`) REFERENCES `journalists` (`id`);
+
+  -- --------------------------------------------------------
+
+  --
+  -- Table structure for table `article_tags`
+  --
+
+
+CREATE TABLE `article_tags` (
+  `article_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+
+
+--
+-- Indexes for table `article_tags`
+--
+ALTER TABLE `article_tags`
+  ADD PRIMARY KEY (`article_id`,`tag_id`),
+  ADD KEY `tag_id` (`tag_id`) USING BTREE;
+
+--
+-- Constraints for dumped tables
+--
 
 --
 -- Constraints for table `article_tags`
--- lib/sql/load.sql
+++ lib/sql/load.sql
@@ -1,33 +1,47 @@
--- phpMyAdmin SQL Dump
--- version 4.7.7
--- https://www.phpmyadmin.net/
---
--- Host: localhost:8889
--- Generation Time: May 23, 2018 at 04:15 AM
--- Server version: 5.6.38
--- PHP Version: 7.2.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
---
--- Database: `news_db`
---
 -- Dumping data for table `journalists`
 --
 
 INSERT INTO `journalists` (`id`, `login`, `firstname`, `lastname`, `privilege`, `hashed_password`, `salt`) VALUES
-(1, 'kris', 'Bigg', 'Boss', 2, '80b03549f7879a84ebb04bd46fde4132a5bedac49d180c0cf0aee105e0846f0a', '5802A39F64CD7BE1'),
-(2, 'kris2', 'John', 'Journo', 1, 'b6be06a139be0809f80ee8f18b974b79d56d9aef3f20b7e381c72a47686e4597', '69DB587F10AE324C');
+(1, 'admin', 'Peter', 'Parker', 2, 'f1c827fb69dfc45fe7f8779c9a828b5b5db267f8dee71c18555c8fcf63870182', '09E21687DA54C3FB'),
+(2, 'journo1', 'Ray', 'Martinez', 1, '8aa9d9f29afbf223d747e2cba8a4b662f7d6382c49d37b3ff46c2043230785c1', '1289450D67AECFB3');
+
+--
+-- Dumping data for table `tags`
+--
+
+INSERT INTO `tags` (`tag_id`, `tag_text`) VALUES
+(1, 'Australia'),
+(2, 'World'),
+(3, 'Technology'),
+(4, 'Sport'),
+(5, 'General');
 
 --
 -- Dumping data for table `articles`
 --
 
 INSERT INTO `articles` (`article_id`, `headline`, `data`, `created_by`, `created_at`, `updated_at`) VALUES
-(1, 'First Headline', 'This is the first headline for my online newspaper', 1, '2018-05-18 19:18:22', '2018-05-19 18:13:08'),
-(3, 'Second Headline', 'testing add data', 2, '2018-05-20 17:56:43', '2018-05-20 17:56:43'),
-(4, 'Headline3', 'Testing add data again', 2, '2018-05-20 17:57:13', '2018-05-20 17:57:13'),
-(5, 'Another article to test', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae odio neque. Nullam accumsan quam nibh, nec suscipit arcu molestie non. Sed at mi nec nisi varius mattis et eget elit. Pellentesque blandit est quis dui auctor, pulvinar lacinia ante aliquam. Aenean vehicula sapien vel varius eleifend. Phasellus accumsan, elit eu consectetur suscipit, massa tellus tincidunt arcu, sed pellentesque odio dui eget enim. Suspendisse accumsan ultrices iaculis. In non quam eu metus bibendum interdum. Vestibulum luctus tempor sollicitudin. Nulla dictum sodales eros, sit amet consectetur sapien finibus a. Donec eros massa, gravida eu lacus eget, maximus pretium libero.', 2, '2018-05-22 01:46:33', '2018-05-22 01:46:33');
+(1, 'Canadian Royal Wedding', '  In sodales pulvinar massa, at bibendum augue suscipit sed. Integer pharetra lectus vitae massa tempus, eu euismod velit interdum. Suspendisse efficitur nec libero ut dapibus. Ut a purus turpis. Praesent molestie elit id tortor vulputate, eleifend accumsan magna bibendum. Quisque pretium porta mauris et maximus. Vestibulum id varius purus, in pulvinar quam. In laoreet urna placerat erat rhoncus lacinia. In hac habitasse platea dictumst.\r\n\r\n', 1, '2018-05-26 03:27:53', '2018-05-26 04:44:07'),
+(2, 'Big Game Grand Final', '  Vestibulum vitae semper tortor, varius aliquet lorem. Nullam magna orci, venenatis ut pellentesque id, laoreet ac velit. Duis vel blandit nibh. Donec semper pretium turpis sit amet laoreet. Nullam euismod molestie ipsum, vitae volutpat ligula ultricies a. Nullam volutpat posuere urna, vel laoreet sem pellentesque non. Sed non imperdiet diam, dignissim congue lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec ornare lorem ut turpis hendrerit luctus. Nulla facilisis, arcu sed lacinia egestas, dui ipsum pellentesque justo, quis scelerisque ante mauris ac ex. Sed sed odio leo. Nullam eget libero nec nisi fermentum vehicula eget lacinia augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non enim nec dui ornare blandit. Phasellus dignissim vehicula sapien, vel aliquam velit volutpat ut.', 1, '2018-05-26 03:28:36', '2018-05-26 04:44:14'),
+(3, 'Mobile Phone Reviews', '  Mauris pellentesque est nec consectetur laoreet. In sit amet pulvinar dolor. Nullam cursus vulputate lectus non pretium. Nulla tincidunt risus et magna dapibus, et cursus libero sodales. Integer sit amet mi facilisis, vestibulum ipsum eu, ultrices libero. Nulla hendrerit posuere tortor eu hendrerit. Aliquam euismod mollis velit, eu tristique sem elementum a. Pellentesque vitae dolor at lacus venenatis pellentesque. Vestibulum sem risus, efficitur sit amet urna sed, rutrum porta augue. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer a sagittis nulla, mattis imperdiet velit.', 1, '2018-05-26 03:29:35', '2018-05-26 04:44:20'),
+(4, 'Election Results', '  Cras quis molestie tortor, ut elementum urna. Proin hendrerit ultrices magna vel commodo. Cras non nunc sit amet nisi malesuada posuere. Vestibulum id nulla mi. Aliquam hendrerit, augue quis laoreet lacinia, mi nisi commodo magna, ut congue ligula dui at ligula. Sed metus arcu, pharetra viverra feugiat ac, accumsan id mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque tincidunt metus nec est pharetra, id lobortis orci scelerisque. Vestibulum eu purus fermentum, ultrices justo id, scelerisque arcu. Aliquam nec ex tempor, faucibus neque at, pulvinar nunc. Nullam in vestibulum dolor, sed bibendum risus. Nunc eget ornare nunc.', 1, '2018-05-26 03:30:45', '2018-05-26 04:47:34'),
+(5, 'Badminton Scandal', '  Pellentesque mi tortor, iaculis vel rhoncus vitae, tincidunt eget libero. Aenean rutrum sem libero, nec dapibus purus consequat non. In viverra diam sit amet placerat euismod. Aliquam venenatis, nunc eu tempor iaculis, est leo pulvinar libero, ut rhoncus ligula leo id sapien. Quisque volutpat vulputate magna vel fermentum. Mauris quis pharetra magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas finibus risus ac mauris dapibus venenatis. Nam convallis lectus eget diam ornare ultricies a vitae ex.\r\n\r\n', 2, '2018-05-26 03:31:35', '2018-05-26 04:48:18'),
+(6, 'Nuclear War Imminent', '  Integer a sodales justo. Etiam fringilla maximus lectus, eget lobortis augue consectetur pretium. Proin ut odio et leo auctor luctus. Suspendisse lacinia pellentesque aliquet. Vestibulum finibus, metus sit amet venenatis facilisis, nisi mi luctus arcu, ut aliquam enim arcu id nunc. Integer odio nibh, euismod vitae eros vel, ultricies maximus urna. Praesent non laoreet odio, et ullamcorper neque. Praesent dui nibh, accumsan sit amet lorem non, tincidunt pretium ante. Etiam vulputate turpis justo, nec venenatis est ultricies vitae. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi aliquet urna quam, eget convallis nisl molestie ac.', 2, '2018-05-26 03:32:05', '2018-05-26 04:48:22'),
+(7, 'Eggs are Bad Again', '  Nullam ultricies neque a metus congue, vitae venenatis orci molestie. Maecenas feugiat massa id sapien auctor cursus sed iaculis magna. Donec et condimentum tellus. Morbi commodo ligula a turpis pellentesque, eu blandit est efficitur. Maecenas sit amet neque odio. Nullam efficitur feugiat ex id finibus. Maecenas fermentum imperdiet odio a iaculis. Suspendisse lobortis elit purus, id maximus mi feugiat sit amet. Phasellus tristique tortor ut dui rutrum, in interdum tellus consectetur. Vivamus euismod tortor vel malesuada consequat. Aliquam vulputate at ligula non elementum. Nunc vehicula ut urna a hendrerit. Vestibulum risus arcu, malesuada sit amet scelerisque vitae, porta non massa. Mauris faucibus nisl in tellus mollis auctor. Morbi quis ligula aliquet, tincidunt libero non, consequat quam. Cras convallis enim quis metus faucibus placerat.', 2, '2018-05-26 03:32:35', '2018-05-26 04:48:33');
 
+-- Dumping data for table `article_tags`
 --
+
+INSERT INTO `article_tags` (`article_id`, `tag_id`) VALUES
+(2, 1),
+(4, 1),
+(5, 1),
+(1, 2),
+(6, 2),
+(7, 2),
+(2, 4),
+(5, 4),
+(3, 5),
+(4, 5),
+(5, 5),
+(7, 5);
